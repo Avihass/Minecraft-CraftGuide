e39c80efe85ea5cfb3d9e051e69dfec3aa002e3c
Avidan Hassine <avihass@outlook.com>
2018-08-16 15:53:14 +0300

    -Add images to smelting page -Add Smelting swift file

---
+++ MinecraftGuideNew/Controllers/Smelting.swift
@@ -0,0 +1,13 @@
+//
+//  Smelting.swift
+//  MinecraftGuideNew
+//
+//  Created by Avidan Hassine on 16/08/2018.
+//  Copyright © 2018 Avidan Hassine. All rights reserved.
+//
+
+import UIKit
+
+class Smelting: UIViewController {
+    
+}
